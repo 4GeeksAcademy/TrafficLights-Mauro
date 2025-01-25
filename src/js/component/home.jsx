05a4702b40f146b76b6b04e4ee4e7bbc515bde53
MauroG07 <71202727+MauroG07@users.noreply.github.com>
2025-01-25 18:00:52 +0000

    Traffic-Lights-Boostrap-Only

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,24 +1,66 @@
-import React from "react";
+import React, { useState } from "react";
+import Styles from "./styles.css"
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+
+	
+	const [selectedColor, setSelectedColor] = useState('')
+
+
+
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div>
+			<div className="container" style={Styles} id="TF-Top">
+				<div className=" bg-black">
+				</div>
+			</div>
+			<div className="container d-flex justify-content-center">
+				<div className="">
+					<div>
+						<div className="col-12 bg-black text-black d-flex justify-content-center">
+							<div className="col-6 bg-black rounded-circle">
+								margin top
+							</div>
+						</div>
+						<div className="col-12 bg-black text-black d-flex justify-content-center pb-3 position-relative">
+							<div className="col-6 bg-danger rounded-circle">
+								<figure className="figure">
+									<img src="..." className="figure-img img-fluid rounded" alt="" />
+								</figure>
+							</div>
+							<div onClick={() => {setSelectedColor('red')}} className={`spinner-grow text-danger position-absolute w-75 h-75 opacity-${(selectedColor === 'red') ? '100' : '0'}`}  role="status">
+							</div>
+						</div>
+						<div className="col-12 bg-black text-black d-flex justify-content-center pb-3 position-relative">
+							<div className="col-6 bg-warning rounded-circle">
+								<figure className="figure">
+									<img src="..." className="figure-img img-fluid rounded" alt="" />
+								</figure>
+							</div>
+							<div onClick={() => {setSelectedColor('yellow')}} className={`spinner-grow text-warning position-absolute w-75 h-75 opacity-${(selectedColor === 'yellow') ? '100' : '0'}`} role="status">
+							</div>
+						</div>
+						<div className="col-12 bg-black text-black d-flex justify-content-center pb-3 position-relative">
+							<div className="col-6 bg-success rounded-circle">
+								<figure className="figure">
+									<img src="..." className="figure-img img-fluid rounded" alt="" />
+								</figure>
+							</div>
+							<div onClick={() => {setSelectedColor('green')}} className={`spinner-grow text-success position-absolute w-75 h-75 opacity-${(selectedColor === 'green') ? '100' : '0'}`} role="status">
+							</div>
+						</div>
+						<div className="col-12 bg-black text-black d-flex justify-content-center">
+							<div className="col-6 bg-black rounded-circle">
+								margin bottom
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
 		</div>
 	);
 };
